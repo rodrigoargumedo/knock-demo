c9444ca4206fd07861876eb1692e777a9f6bae5c
Rodrigo Argumedo <rodrigo.argumedo@icloud.com>
2017-01-04 04:15:58 +0000

    Add v1/users controller and additional routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :v1 do
+    resources :users, param: 'username' do
+      resources :projects, shallow: true
+    end
+  end
 end
