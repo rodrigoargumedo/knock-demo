48452b545e71a4576d583d354e18cf83ac1f67a6
Rodrigo Argumedo <rodrigo.argumedo@icloud.com>
2017-01-05 02:45:18 +0000

    Install RSpec and add routes

--- Gemfile
+++ Gemfile
@@ -29,6 +29,10 @@ gem 'rack-cors'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+  gem 'shoulda-matchers'
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
+# == Route Map
+#
+#           Prefix Verb   URI Pattern                                 Controller#Action
+# v1_user_projects GET    /v1/users/:user_username/projects(.:format) v1/projects#index
+#                  POST   /v1/users/:user_username/projects(.:format) v1/projects#create
+#       v1_project GET    /v1/projects/:id(.:format)                  v1/projects#show
+#                  PATCH  /v1/projects/:id(.:format)                  v1/projects#update
+#                  PUT    /v1/projects/:id(.:format)                  v1/projects#update
+#                  DELETE /v1/projects/:id(.:format)                  v1/projects#destroy
+#         v1_users GET    /v1/users(.:format)                         v1/users#index
+#                  POST   /v1/users(.:format)                         v1/users#create
+#          v1_user GET    /v1/users/:username(.:format)               v1/users#show
+#                  PATCH  /v1/users/:username(.:format)               v1/users#update
+#                  PUT    /v1/users/:username(.:format)               v1/users#update
+#                  DELETE /v1/users/:username(.:format)               v1/users#destroy
+#
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :v1 do
